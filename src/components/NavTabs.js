4f320cb3e128bf49441f45e6b859be33d0b71d4a
dakenny5241 <123331009+dakenny5241@users.noreply.github.com>
2023-06-16 11:49:02 +0100

    changing About component to Portfolio component -3

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -18,10 +18,10 @@ function NavTabs({ currentPage, handlePageChange }) {
       </li>
       <li className="nav-item">
         <a
-          href="#about"
-          onClick={() => handlePageChange('About')}
-          // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+          href="#portfolio"
+          onClick={() => handlePageChange('Portfolio')}
+          // Check to see if the currentPage is `Portfolio`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
         >
           About
         </a>
