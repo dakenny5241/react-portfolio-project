644b5b10a6171069cb33e985676cdabb844b91e2
dakenny5241 <123331009+dakenny5241@users.noreply.github.com>
2023-06-21 19:26:50 +0100

    projects.json file edited

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -1,7 +1,7 @@
 import React from "react";
-import "./style.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function FriendCard(props) {
+function projectCard(props) {
   return (
     <div className="card">
       <div className="img-container">
@@ -20,11 +20,11 @@ function FriendCard(props) {
           </li>
         </ul>
       </div>
-      <span onClick={() => props.removeFriend(props.id)} className="remove">
+      <span onClick={() => props.removeproject(props.id)} className="remove">
         𝘅
       </span>
     </div>
   );
 }
 
-export default FriendCard;
+export default projectCard;
