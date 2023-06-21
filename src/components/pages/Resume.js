c22fb1adcc99bb0beca770cc2fe2f34e96265c48
dakenny5241 <123331009+dakenny5241@users.noreply.github.com>
2023-06-21 20:17:36 +0100

    Resume file edited

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -12,6 +12,16 @@ export default function Resume() {
             <li>Proficiency 3</li>
           </ul>
         </section>
+        <footer>
+        <a href="https://github.com/dakenny5241" target="_blank" rel="noopener noreferrer">GitHub</a>
+        <a href="https://linkedin.com/dakenny5241" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        <a href="https://stackoverflow.com/dakenny5241" target="_blank" rel="noopener noreferrer">Stack Overflow</a>
+        <a href="https://twitter.com/dakenny5241" target="_blank" rel="noopener noreferrer">Twitter</a>
+        <a href="https://www.instagram.com/dakenny5241/" target="_blank" rel="noopener noreferrer">Instagram</a>
+
+        
+        <p>If you would like to get in touch with me, please feel free to email me at <a href="mailto:dakenny5241@gmail.com">dakenny5241@gmail.com</a>.</p>
+      </footer>
     </div>
   );
-}
+};
