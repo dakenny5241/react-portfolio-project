5f05bd8abc984f5dee0307fc3f33f75926be83f1
dakenny5241 <123331009+dakenny5241@users.noreply.github.com>
2023-06-24 16:05:52 +0100

    style.css file updated

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -20,6 +20,7 @@ export default function PortfolioContainer() {
       return <Contact />;
     }
     return <Resume />;
+    
   };
 
   const handlePageChange = (page) => setCurrentPage(page);
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -5,18 +5,33 @@ export default function Contact() {
   return (
     <div>
     <section id="contact">
-          <h2>Contact</h2>
-          <form>
-            <label for="name">Name:</label>
-            <input type="text" name="name" required />
-            <label for="email">Email:</label>
-            <input type="email" name="email" required />
-            <label for="message">Message:</label>
-            <textarea name="message" required></textarea>
-            <input type="submit" value="Send Message" />
-          </form>
-        </section>   
-      <p></p>
-    </div>
+  <h2>Contact</h2>
+  <ul>
+    <li>
+      <form>
+        <label for="name">Name:</label>
+        <input type="text" name="name" required />
+      </form>
+    </li>
+    <li>
+      <form>
+        <label for="email">Email:</label>
+        <input type="email" name="email" required />
+      </form>
+    </li>
+    <li>
+      <form>
+        <label for="message">Message:</label>
+        <textarea name="message" required></textarea>
+      </form>
+    </li>
+    <li>
+      <form>
+        <input type="submit" value="Send Message" />
+      </form>
+    </li>
+  </ul>
+</section>
+</div>
   );
-}
+};
