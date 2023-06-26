d7a757b1999cb3df8e84c789ea03ff600dcc23b1
dakenny5241 <123331009+dakenny5241@users.noreply.github.com>
2023-06-26 13:47:30 +0100

    images-logo added

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -25,11 +25,10 @@ export default function Resume() {
         </section>
         <footer>
         <h2>Contact Me</h2>
-        <a href="https://github.com/dakenny5241" target="_blank" rel="noopener noreferrer">GitHub</a>
-        <a href="https://www.linkedin.com/in/nkem-okoro-8677b4207" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-        <a href="https://stackoverflow.com/dakenny5241" target="_blank" rel="noopener noreferrer">Stack Overflow</a>
-        <a href="https://twitter.com/dakenny5241" target="_blank" rel="noopener noreferrer">Twitter</a>
 
+        <a href="https://www.linkedin.com/in/nkem-okoro-8677b4207" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        <a href="https://twitter.com/dakenny5241" target="_blank" rel="noopener noreferrer">Twitter</a>
+        <a href="https://github.com/dakenny5241" target="_blank" rel="noopener noreferrer">GitHub</a>
 
         
         <p>If you would like to get in touch with me, please feel free to email me at <a href="mailto:dakenny5241@gmail.com">dakenny5241@gmail.com</a>.</p>
