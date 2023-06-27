40dcfb8c0a086d2d7efb2b1af1203583d5144cac
dakenny5241 <123331009+dakenny5241@users.noreply.github.com>
2023-06-27 11:27:30 +0100

    resume.js file created and coded

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./resume.css";
 export default function Resume() {
   return (
     <div>
@@ -26,12 +28,15 @@ export default function Resume() {
         <footer>
         <h2>Contact Me</h2>
 
+<div className="footer"> 
         <a href="https://www.linkedin.com/in/nkem-okoro-8677b4207" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        <br/>
         <a href="https://twitter.com/dakenny5241" target="_blank" rel="noopener noreferrer">Twitter</a>
+        <br/>
         <a href="https://github.com/dakenny5241" target="_blank" rel="noopener noreferrer">GitHub</a>
 
-        
         <p>If you would like to get in touch with me, please feel free to email me at <a href="mailto:dakenny5241@gmail.com">dakenny5241@gmail.com</a>.</p>
+        </div>
       </footer>
     </div>
   );
