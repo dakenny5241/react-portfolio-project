b44eca5ba39fdffc5d7b267af3e34d853561cb79
dakenny5241 <123331009+dakenny5241@users.noreply.github.com>
2023-08-18 13:01:24 +0100

    AboutMe.js edited

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -9,7 +9,7 @@ export default function AboutMe() {
       <img src={process.env.PUBLIC_URL + kennyPic} className=" float-left myPic" alt="Kenny Okoro" />
      
       <p>
-      My name is Nkem Okoro and I am a web developer. I have been working in the web development field for the past 5 months and have a passion for creating beautiful and functional websites. I am looking for a part-time or full-time position where I can continue to grow my skills and contribute to the success of the company
+      My name is Nkem Okoro and I am a web developer. I have been working in the web development field for the past 6 months and have a passion for creating beautiful and functional websites. I am looking for a part-time or full-time position where I can continue to grow my skills and contribute to the success of the company
       </p>
     </div>
   );
